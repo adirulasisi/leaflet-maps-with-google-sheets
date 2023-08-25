028e066fa36007f55b310be34b39c178fe67c41d
adirulasisi <102596830+adirulasisi@users.noreply.github.com>
2023-08-25 22:47:39 +0000

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1YBGabvTw5VYOukxExhBlz_HackzHnhYyFaL--V5_Eq8/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyDv-_VbqxyuAFy1yQL--f4pgoBhmDr51BY';
